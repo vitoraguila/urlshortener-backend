86e8f609178f3286e4456b354de5a87c34697658
Vitor Aguila <vitor.aguila1@gmail.com>
2020-03-26 00:27:28 +0100

    set cors and readme file

---
+++ readme.md
@@ -0,0 +1,8 @@
+# URL Shortener - Backend
+
+
+
+#### **More informations:** 
+
+https://medium.com/@vitoraguila/creating-a-url-shortener-with-reactjs-typescript-nodejs-mongodb-docker-from-scratch-part-f127260e444a
+
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import mongoDBConnect from "./database";
 import routes from "./routes";
+import cors from "cors";
 
 const app = express();
 mongoDBConnect();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3001);
+app.listen(3003);
